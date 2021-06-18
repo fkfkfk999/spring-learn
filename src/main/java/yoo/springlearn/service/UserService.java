9f0d55d7f0d1e8a5514448542a9dc67e34ec6722
fkfkfk999 <fkfkfk9@daum.net>
2021-06-18 21:27:04 +0900

    Update UserService.java

--- src/main/java/yoo/springlearn/service/UserService.java
+++ src/main/java/yoo/springlearn/service/UserService.java
@@ -16,12 +16,25 @@ public class UserService {
     */
     public Long join(UserVO vo){
         //중복회원 이름으로 체크
+        validateDuplicateUser(vo);
+        userRepository.insertUser(vo);
+        return vo.getSerialNo();    
+    }
+
+    private void validateDuplicateUser(UserVO vo){
         //ifPresent는 null이 아닌 값이 있으면 동작한다.
+        //orElseGet도 많이 사용된다. 값이 있으면 꺼내고 없으면 실행시킨다.
         userRepository.findByName(vo.getUserName()).ifPresent(m -> {
             throw new IllegalStateException("이미 존제하는 회원입니다.");
         });
+    }
 
-        userRepository.insertUser(vo);
-        return vo.getSerialNo();    
+    // 전체 회원 조회
+    public List<UserVO> findMembers() {
+        return userRepository.findAll();        
+    }
+
+    public Optional<UserVO> findOne(Long seriaNo){
+        return userRepository.findBySerialNo(serialNo);
     }
 }
